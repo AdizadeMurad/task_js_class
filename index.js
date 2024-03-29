5e2fad1384c6ab20c9624c672a295cc9d51ff017
AdizadeMurad <bd7x445u@code.edu.az>
2024-03-29 19:33:13 +0400

    add

---
+++ index.js
@@ -0,0 +1,173 @@
+// // //expression
+// // let checkofodd = (num)=> {
+// //    if(num%2===0){
+// //     return "true"
+// // }
+    
+// // else {
+// //     return "false"
+// // }
+// // }
+// // console.log(checkofodd(222));
+
+
+
+
+
+// // let checkofodd2 = function(num) {
+// //     if(num%2===0){
+// //      return "true"
+// //  }
+     
+// //  else {
+// //      return "false"
+// //  }
+ 
+// //  }
+// //  console.log(checkofodd2(222));
+ 
+
+
+
+
+// // function checkofodd3(num) {
+// //         if(num%2===0){
+// //      return "true"
+// //  }
+     
+// //  else {
+// //      return "false"
+// //  }
+// //  }
+
+// //  console.log(checkofodd3(222));
+
+
+
+
+
+
+
+
+
+// // function show(name,name2,...parms) {
+// //     console.log("test1" + name);
+// //     console.log(parms);
+// // }
+// // show(2,3,4,5,6,7)
+
+
+
+
+
+
+
+// // 1.
+
+
+// // const arr1 = [1,2,3,4,5]
+// // const arr2 = [5, 6, 7, 8]
+// // function show(arr) {
+    
+// // }
+// // console.log(arr1.concat(arr2))
+
+
+
+// // const num1 =[1,2,3,4]
+// // const num2=[4,5,6,7]
+
+
+// // function name(num1,num2) {
+// //     for (let i = 0; i< num2.length; i++) {
+        
+// //      num1.push(num2[i])
+        
+// //     }
+// //    console.log(num1);
+// // } 
+
+// // name([1,2,3,4,5],[6,7,8,9])
+
+
+
+
+
+// // const arr = []
+// // function name(num1,num2) {
+// //     for (let i = 0; i< num1; i++) {
+// //         arr.push(num2)
+ 
+        
+// //     }
+// //     console.log(arr);
+// // }
+// // name(6,0)
+
+
+// // 3
+
+
+
+// // function show(num1,currentIndex,nextIndex) {
+// // let element=num1[currentIndex] 
+// //    num1.splice(currentIndex,1)
+// //    num1.splice(nextIndex,0,element)
+// //    return num1
+// // }
+
+
+// // console.log(show([10, 20, 30, 40, 50],3,0))
+
+
+
+// // 4
+
+// // const test = []
+// // function name(num1,num2) {
+// //     for (let i = num1; i<= num2; i++) {
+        
+// //         test.push(i)
+// //     }
+// //    console.log(test);
+// // } 
+// // name(1,4)
+
+
+// // 5.
+
+// // const armud = []
+
+// //  function show(arr) {
+// //    for (let i = 0; i< arr.length; i++) {
+// //     if(!armud.includes(arr[i])){
+// //         armud.push(arr[i])
+// //     }
+    
+// //    }
+// // console.log(armud);
+// //  }
+// //  show([1,2,3,4,4,5,6,7,8])
+
+
+
+// const armud = []
+
+// function show(arr1,arr2) {
+
+//     for (let i = 0; i < arr1.length; i++) {
+       
+//      armud.push(arr1[i])
+//     }
+   
+//     for (let i = 0; i < arr2.length; i++) {
+     
+//        armud.push(arr2[i])   
+//     }  
+    
+// if (arr1[i]!==arr2[2]) {
+    
+// }
+// console.log(armud);
+// }
+// show([1, 2, 3], [100, 2, 1, 10])
